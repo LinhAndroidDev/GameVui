74f58a18c7e97a27348ba0c15591f4a0191bb327
LinhAndroidDev <nguyenlinh07082001bn@gmail.com>
2023-01-02 23:10:04 +0700

    02/01

--- app/src/main/java/com/example/gamevui/SecondActivity.java
+++ app/src/main/java/com/example/gamevui/SecondActivity.java
@@ -4,20 +4,37 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    TextView here;
     ImageButton iconCaro,iconDice,iconRace;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        iconCaro=findViewById(R.id.iconCaro);
-        iconDice=findViewById(R.id.iconDice);
-        iconRace=findViewById(R.id.iconRace);
+        iconCaro = findViewById(R.id.iconCaro);
+        iconDice = findViewById(R.id.iconDice);
+        iconRace = findViewById(R.id.iconRace);
+        here = findViewById(R.id.gameOther);
+
+        here.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.gamevui.vn/"));
+                startActivity(intent);
+            }
+        });
+
+        here.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
 
         Intent intentCaro=new Intent();
         Intent intentDice=new Intent();
